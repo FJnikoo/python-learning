8aa4b0cf33912cc4b8f88ceb3966d10667ac4c8d
FJnikoo <140260376+FJnikoo@users.noreply.github.com>
2026-08-24 16:10:29 +0200

    Add student list and print scores

---
+++ day-06/list_dic.py
@@ -0,0 +1,8 @@
+students = [
+    {"name": "Ali", "score": 85 },
+    {"name": "Sara", "score": 92 },
+    {"name": "Robert", "score": 60 }
+]
+for student in students:
+    print(f"{student['name']} - {student['score']}")
+   
